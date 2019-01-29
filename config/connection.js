bdbd594393ab66b4a811ecfb8e61097f4be289d4
Wufasa <mxw373@case.edu>
2019-01-29 14:58:28 -0800

    heroku changes

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,7 @@ if (process.env.JAWSDB_URL){
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
+        port: 3306,
         password: "password",
         database: "burgers_db"
     })
